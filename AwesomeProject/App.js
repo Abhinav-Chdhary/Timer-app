492e1a0fcf1c502cb4468eb4280dbd81042ffcc4
Abhinav Choudhary <abhinavchdhary@gmail.com>
2023-10-11 19:37:02 +0530

    Update App.js
    Just messing around learning

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,12 +1,23 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, TextInput, View } from "react-native";
+import {
+  Button,
+  Pressable,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World! Fuck you</Text>
-      <TextInput>input</TextInput>
-      <StatusBar style="auto" />
+      <Text style={styles.whiteinator}>00 : 00</Text>
+      {/* <Button title="START" color={"red"} />
+      <Button title="PRESS ME" color={"blue"} /> */}
+      <Pressable style={styles.startStopButton}>
+        <Text style={styles.startStopText}>START</Text>
+      </Pressable>
+      <StatusBar style="auto" backgroundColor="#ffffff" />
     </View>
   );
 }
@@ -14,8 +25,25 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#f00",
+    backgroundColor: "#020936",
     alignItems: "center",
     justifyContent: "center",
+  },
+  whiteinator: {
+    color: "white",
+    fontSize: 30,
+  },
+  startStopButton: {
+    alignItems: "center",
+    justifyContent: "center",
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: "red",
+  },
+  startStopText: {
+    color: "#020936",
+    fontSize: 20,
   },
 });
